e8593a26f27e3b68d4898882004df231049b1017
Saumye <saumye.mehrotra96@gmail.com>
2020-03-29 13:54:00 +0530

    added the journey model class that has the details of the journey of a particular user

---
+++ src/main/java/com/railway/model/Journey.java
@@ -0,0 +1,26 @@
+package com.railway.model;
+
+public class Journey {
+	private String source;
+	private String destination;
+	private Character type;
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public Character getType() {
+		return type;
+	}
+	public void setType(Character type) {
+		this.type = type;
+	}
+	
+}
