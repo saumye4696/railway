895449ffe03569a7225997f7982893003546fb9d
Saumye <saumye.mehrotra96@gmail.com>
2020-03-29 13:53:05 +0530

    added the user model class

--- src/main/java/com/railway/model/User.java
+++ src/main/java/com/railway/model/User.java
@@ -1,5 +1,33 @@
 package com.railway.model;
 
 public class User {
-
+	private String name;
+	private Integer age;
+	private Integer noOfTickets;
+	private Journey journey;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Integer getNoOfTickets() {
+		return noOfTickets;
+	}
+	public void setNoOfTickets(Integer noOfTickets) {
+		this.noOfTickets = noOfTickets;
+	}
+	public Journey getJourney() {
+		return journey;
+	}
+	public void setJourney(Journey journey) {
+		this.journey = journey;
+	}
+	
 }
