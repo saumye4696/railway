a3723208f949eba83756684ef63f7185f2450b7b
Saumye <saumye.mehrotra96@gmail.com>
2020-04-11 15:57:40 +0530

    some changes

--- src/main/java/com/railway/webApp/WebAppApplication.java
+++ src/main/java/com/railway/webApp/WebAppApplication.java
@@ -2,14 +2,14 @@ package com.railway.webApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebAppApplication {
+public class WebAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebAppApplication.class, args);
 	}
 
 }
-		//package com.railway.webApp;
-		
+	
