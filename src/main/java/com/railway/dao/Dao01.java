3a0c214e75d2ff3a427af765f69bdc1b36bfc550
Saumye <saumye.mehrotra96@gmail.com>
2020-04-11 14:53:08 +0530

    added the dao implementation

---
+++ src/main/java/com/railway/dao/Dao01.java
@@ -0,0 +1,24 @@
+package com.railway.dao;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Repository;
+
+@Repository(value = "userDao")
+public class Dao01 {
+	public List<String> showPlaces(String name) {
+		List<String> l1 = new ArrayList<String>();
+		
+		//creating a result list to return 
+		l1.add("delhi");
+		l1.add("bangalore");
+		l1.add("mysore");
+		if(name.equals("saumye")) {
+			return l1;
+		}
+		else {
+			return null;
+		}
+	}
+}
